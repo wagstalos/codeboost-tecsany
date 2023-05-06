8ec1005fc43f40f3996ba1b535857b44c8e9d2ad
Wagner Paulo <wagstalos@hotmail.com>
2023-05-06 10:34:45 -0300

    add close mobile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-const botoes = document.getElementById("menu-mob");
+const btn = document.getElementById("menu-mob");
 const menuMobile = document.getElementById("menu-mobile");
 
-botoes.addEventListener("click", () => {
+
+btn.addEventListener("click", () => {
   menuMobile.classList.toggle("active");
+
+  // const closeMenuMobile = document.getElementById("close");
+
+  // if (menuMobile.classList.contains('active')) {
+  //   // fazer algo se tiver a classe "active"
+  //   alert('tem')
+  //   closeMenuMobile.classList.add('menu-visivel');
+  // } else {
+  //   // fazer algo se não tiver a classe "active"
+  //   alert('nao tem')
+  // }
 });
+
 
 
 //Swiper
@@ -12,35 +25,36 @@ const swiper = new Swiper(".swiper", {
   speed: 400,
   spaceBetween: 100,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 });
 
-
-const backgroundMain = document.getElementById('main');
-const elipseGreen = document.getElementsByClassName('elipse-green');
-swiper.on('slideChange', function() {
+const backgroundMain = document.getElementById("main");
+const elipseGreen = document.getElementsByClassName("elipse-green");
+swiper.on("slideChange", function () {
   // Verifica se o slide atual é o último slide
   if (swiper.isEnd) {
     // Executa algo quando o último slide é exibido
-    console.log('O último slide foi exibido!');
-    elipseGreen[0].style.display = 'none';
-    backgroundMain.style.background = 'linear-gradient(360deg, #FFF0F0 0%, rgba(255, 231, 231, 0) 101.57%)';
-  }else{
-    elipseGreen[0].style.display = 'block';
-    backgroundMain.style.background = 'linear-gradient(360deg, #F5F9F3 0%, rgba(245, 249, 243, 0) 101.57%)';
+    console.log("O último slide foi exibido!");
+    elipseGreen[0].style.display = "none";
+    backgroundMain.style.background =
+      "linear-gradient(360deg, #FFF0F0 0%, rgba(255, 231, 231, 0) 101.57%)";
+  } else {
+    elipseGreen[0].style.display = "block";
+    backgroundMain.style.background =
+      "linear-gradient(360deg, #F5F9F3 0%, rgba(245, 249, 243, 0) 101.57%)";
   }
 });
 
-let nextButton = document.getElementById('swiper-button-next');
-let prevButton = document.getElementById('swiper-button-prev');
+let nextButton = document.getElementById("swiper-button-next");
+let prevButton = document.getElementById("swiper-button-prev");
 
-nextButton.addEventListener('click', function() {
+nextButton.addEventListener("click", function () {
   // Avança para o próximo slide quando o botão é clicado
   swiper.slideNext();
 });
 
-prevButton.addEventListener('click', function() {
+prevButton.addEventListener("click", function () {
   // Avança para o próximo slide quando o botão é clicado
   swiper.slidePrev();
 });
